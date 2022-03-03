138c36d6357c68f992ed5b6e6b0e5b20ee1139ff
Fabio Conti <e_ternauta@live.it>
2022-03-03 12:11:31 +0100

    bug photo not resolve

--- src/components/SetCamera.js
+++ src/components/SetCamera.js
@@ -4,31 +4,32 @@ import IconButton from "@mui/material/IconButton";
 import CameraAltRoundedIcon from "@mui/icons-material/CameraAltRounded";
 import FlipCameraIosRoundedIcon from "@mui/icons-material/FlipCameraIosRounded";
 import CameraRoundedIcon from "@mui/icons-material/CameraRounded";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { storage } from "../config/firebase_config";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
 import moment from "moment";
-import Box from "@mui/material/Box";
+import { Box, Button, Typography } from "@mui/material";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Container from "@mui/material/Container";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Button from "@mui/material/Button";
 
 function SetCamera({ user, cinema, item, setItem }) {
   const [open, setOpen] = useState(false);
-  const [photosLink, setPhotoLinks] = useState(null);
+
+  const [blob, setBlob] = useState();
+  const [images, setImages] = useState([]);
 
   /* console.log("photo", user, cinema); */
-  const handleClick = () => {
-    setOpen((prev) => !prev);
-    console.log(open)
-  };
 
-  const handleClickAway = () => {
-    setOpen(false);
-  };
   const styles = {
     position: "absolute",
     top: 28,
@@ -37,7 +38,7 @@ function SetCamera({ user, cinema, item, setItem }) {
     zIndex: 1,
     border: "1px solid",
     p: 1,
-    bgcolor: "background.paper"
+    bgcolor: "background.paper",
   };
 
   function loadXHR(url) {
@@ -63,135 +64,208 @@ function SetCamera({ user, cinema, item, setItem }) {
     });
   }
 
-  const handleTakePhoto = async (photoUri) => {
+  const onTakePhotoHandler = (photoUri) => {
     //metto la foto in array locale per visulizzarle
-    const snapTime = moment().format("MMMM Do YYYY h:mm:ss a")
+    if (!item.item_ref) {
+      return alert("scegli prima un cienam");
+    }
+    /* console.log(photoUri); */
+    if (images.length > 2) {
+      return alert("al massimo puoi memorizzare 3 photo");
+    }
+    const snapTime = moment().format("MMMM Do YYYY h:mm:ss a");
+    const name = `${cinema.name}/${item.item_ref}/${item.item_ref}-${snapTime}.jpg`;
 
-    const imageRef = ref(
-      storage,
-      `${cinema.name}/${item.item_ref}/${item.item_ref}-${snapTime}.jpg`
-    );
-
-
-
-
-
-
-    //creao array di percorsi
-    console.log("path", imageRef.toString());
-    let newArrayApp = item.photos;
-    newArrayApp.push(imageRef.fullPath);
-    console.log("array item in photo", item);
-    setItem({ ...item, photos: newArrayApp });
-    console.log("array item", item.photos)
-
-
-    //setto i metadata
-    const metadata = {
-      contentType: "image/jpg",
-      customMetadata: {
-        item_ref: `${item.item_ref}`,
-        makeBy: `${user.name}`,
-        takeAt: snapTime
-      }
+    const newObj = {
+      name,
+      photo: photoUri,
     };
 
-    //carico il file
-    await loadXHR(photoUri).then(function (blob) {
-      // here the image is a blob
-      uploadBytes(imageRef, blob, metadata).then((snapshot) => {
-        console.log("Uploaded a blob or file!");
+    const newImages = [...images];
+
+    newImages.push(newObj);
+
+    console.log("newimages", newImages);
+    setImages(newImages);
+  };
+
+  const uploadImages = () => {
+    console.log(images);
+    const urlArray = [];
+
+    for (const i in images) {
+      let obj = images[i];
+      console.log("obj", obj);
+      loadXHR(obj.photo).then(function (blob) {
+        // here the image is a blob
+        setBlob(blob);
       });
-    });
 
-  }
+      const imageRef = ref(storage, obj.name);
+      const metadata = {
+        contentType: "image/jpeg",
 
-  const takePhoto = async () => {
-    console.log("winner in take photo");
+        customMetadata: {
+          name: `${obj.name}`,
+          author: `${user.name}`,
+        },
+      };
+      const uploadTask = uploadBytesResumable(imageRef, blob, metadata);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 
-    if (cinema) {
-      setPhotoLinks([]);
-      const path = `${cinema.name}/${item.item_ref}`;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
 
-      const listRef = ref(storage, `${path}`);
+            // ...
 
-      // Find all the prefixes and items.
-      await listAll(listRef)
-        .then((res) => {
-          res.prefixes.forEach((folderRef) => {
-            console.log("ref", folderRef.toString());
-            // All the prefixes under listRef.
-            // You may call listAll() recursively on them.
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+            default:
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            urlArray.push(downloadURL);
           });
-          res.items.forEach((itemRef) => {
-            /* console.log(itemRef.toString()); */
-            getDownloadURL(ref(storage, `${itemRef}`))
-              .then((url) => {
-                console.log("url", url);
-                setPhotoLinks((e) => [...e, url]);
+        }
+      );
+    }
+    setItem({ ...item, photos: urlArray });
+  };
 
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-            // All the items under listRef.
-          });
-        })
-        .catch((error) => {
-          console.log(error)
-          // Uh-oh, an error occurred!
-        });
+  const removePhoto = (index) => {
+    console.log(index);
+    const arrayApp = [...images];
+    arrayApp.splice(index, 1);
+
+    setImages(arrayApp);
+  };
+
+  const ViewCamera = () => {
+    if (open) {
+      return (
+        <Container
+          sx={{
+            p: 2,
+            m: 2,
+            display: "flex",
+            height: "400",
+            width: "640",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <Camera
+            onTakePhoto={onTakePhotoHandler}
+            idealFacingMode="environment"
+            isImageMirror={false}
+            idealResolution={{ width: 300, height: 300 }}
+          />
+        </Container>
+      );
+    } else {
+      return null;
     }
   };
 
+  const openCamera = () => {
+    setOpen(!open);
+  };
+
   useEffect(() => {
-    takePhoto();
-    /* console.log("photolink", photosLink); */
+    setImages([]);
+    setOpen(false);
+    return () => {
+      setOpen(false);
+    };
   }, [item.item_ref]);
 
   return (
     <Container>
-      <ClickAwayListener onClickAway={handleClickAway}>
-        <Box sx={{ position: "relative" }}>
-          <Button type="button" onClick={handleClick}>
-            Open Camera
-          </Button>
-
-          {open && cinema ? (
-            <>
-              <Box sx={styles}>
-                <Camera
-                  idealResolution={{ width: 300, height: 300 }}
-                  onTakePhoto={(photoUri) => {
-                    handleTakePhoto(photoUri);
-                  }}
-                />
-              </Box>
-            </>
-          ) : null}
-        </Box>
-      </ClickAwayListener>
-      <Box
+      <Container
         sx={{
           display: "flex",
+          flexDirection: "row",
           justifyContent: "center",
-          p: 1,
-          m: 1,
-          bgcolor: "background.paper",
-          borderRadius: 1,
-          width: "auto"
         }}
       >
-        {photosLink && photosLink.length ? (
-          <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-            {photosLink.map((item, key) => (
-              <ImageListItem key={key}>
-                <img src={item} srcSet={item} alt={item.title} loading="lazy" />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        ) : <h2>no image for this item</h2>}
-      </Box>
+        <IconButton
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+          onClick={openCamera}
+          aria-label="openCamera"
+          size="large"
+        >
+          {!open ? <p>open camera </p> : <p>close camera </p>}
+          <CameraRoundedIcon />
+        </IconButton>
+      </Container>
+
+      <ViewCamera />
+
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {images.map((obj, index) => (
+          <Container key={index} sx={{ width: "150px", paddingRight: "12px" }}>
+            <img alt="cam" src={obj.photo} style={{ width: "100%" }} />
+            <IconButton
+              onClick={() => removePhoto(index)}
+              aria-label="delete"
+              size="small"
+              sx={{ m: -1 }}
+            >
+              <DeleteIcon fontSize="inherit" />
+            </IconButton>
+          </Container>
+        ))}
+      </Container>
+      {images.length ? (
+        <Button
+          onClick={uploadImages}
+          variant="contained"
+          color="success"
+          sx={{ m: 2 }}
+        >
+          UPLOAD
+        </Button>
+      ) : null}
     </Container>
   );
 }
